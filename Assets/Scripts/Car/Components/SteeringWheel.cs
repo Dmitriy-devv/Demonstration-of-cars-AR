d636fcc4bce916f222274de760c221e6977430a4
Dmitry Malinkin <malinkin20001@gmail.com>
2022-05-07 14:36:14 +0300

    Added steering wheel like car component

--- Assets/Scripts/Car/Components/SteeringWheel.cs
+++ Assets/Scripts/Car/Components/SteeringWheel.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 namespace Cars
 {
     [RequireComponent(typeof(AudioSource))]
-    public class Accumulator : CarComponent
+    public class SteeringWheel : CarComponent
     {
-        public event Action<bool> Changed;
-        public bool State => _state;
-
         private AudioSource _audioSource;
-        private bool _state;
 
         public override void Init(ComponentSign sign, ComponentLine line)
         {
@@ -22,9 +17,8 @@ namespace Cars
 
         protected sealed override void OnClick()
         {
-            _state = !_state;
-            if (_state) _audioSource.Play();
-            else _audioSource.Stop();
+            //Check Accum
+            _audioSource.Play();
         }
     }
 }
