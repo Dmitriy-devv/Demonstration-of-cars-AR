bc9f58f230aef6f8eb7b63563867b59f28511c71
Dmitry Malinkin <malinkin20001@gmail.com>
2022-05-07 13:44:10 +0300

    Back wheels interact action

---
+++ Assets/Scripts/Car/Components/BackWheel.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars
+{
+    public class BackWheel : CarComponent
+    {
+        [SerializeField] private float _speed;
+        [SerializeField] private float _maxSpeed;
+
+        [SerializeField] private WheelCollider _wheelCollider;
+        [SerializeField] private Transform _wheelTransform;
+        [SerializeField] private BackWheel _otherWheel;
+
+        private float _t;
+
+        public override void Init(ComponentSign sign, ComponentLine line)
+        {
+            base.Init(sign, line);
+            
+        }
+
+        protected override void OnHold()
+        {
+            _t += Mathf.Clamp(_t + Time.deltaTime * 2f *_speed, 0f, 1f);
+            _otherWheel.UpdateSpeed(_t);
+        }
+
+        private void Update()
+        {
+            _wheelCollider.motorTorque = _t * _maxSpeed;
+            transform.position = _wheelTransform.position;
+            if (_currentLine != null) _currentLine.UpdateLine();
+            _t = Mathf.Clamp(_t - Time.deltaTime * _speed, 0f, 1f);
+        }
+
+        public void UpdateSpeed(float value)
+        {
+            _t = value;
+        }
+
+
+    }
+}
