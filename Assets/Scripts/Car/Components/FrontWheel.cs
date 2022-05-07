b86506c1c37586421ea96324667f35cf4d9fb001
Dmitry Malinkin <malinkin20001@gmail.com>
2022-05-07 12:40:20 +0300

    Front wheel interact action

---
+++ Assets/Scripts/Car/Components/FrontWheel.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cars
+{
+    public class FrontWheel : CarComponent
+    {
+        [SerializeField] private float _speedRotate;
+
+        [SerializeField] private WheelCollider _wheelCollider;
+        [SerializeField] private FrontWheel _otherWheel;
+        [SerializeField] private float _maxAngle;
+
+        private bool _direction = false;
+        private float _angle;
+
+        public override void Init(ComponentSign sign, ComponentLine line)
+        {
+            base.Init(sign, line);
+            _angle = 0f;
+
+        }
+
+        protected override void OnHold()
+        {
+            var delta = Time.deltaTime * _speedRotate;
+            _angle = _direction ? _angle + delta: _angle - delta;
+            if (_angle > _maxAngle) _direction = false;
+            if (_angle < -_maxAngle) _direction = true;
+            SetRotation(_angle, _direction);
+            _otherWheel.SetRotation(_angle, _direction);
+            
+        }
+
+        public void SetRotation(float value, bool direction)
+        {
+            _angle = value;
+            _direction = direction;
+            _wheelCollider.steerAngle = _angle;
+            if (_currentLine != null) _currentLine.UpdateLine();
+        }
+    }
+}
