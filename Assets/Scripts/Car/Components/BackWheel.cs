4dc175a8c68cd54cd38863e3e650643c1a997f3f
Dmitry Malinkin <malinkin20001@gmail.com>
2022-05-08 19:56:00 +0300

    Wheel rotation act on car

--- Assets/Scripts/Car/Components/BackWheel.cs
+++ Assets/Scripts/Car/Components/BackWheel.cs
@@ -8,9 +8,11 @@ namespace Cars
     {
         [SerializeField] private float _speed;
         [SerializeField] private float _maxSpeed;
+        [SerializeField] private float _force;
 
         [SerializeField] private WheelCollider _wheelCollider;
         [SerializeField] private Transform _wheelTransform;
+        [SerializeField] private Transform _forcePosition;
         [SerializeField] private BackWheel _otherWheel;
         [SerializeField] private ParticleSystem _particleSystem;
 
@@ -25,6 +27,7 @@ namespace Cars
         protected override void OnHold()
         {
             _t += Mathf.Clamp(_t + Time.deltaTime * 2f *_speed, 0f, 1f);
+            _car.AddForce(_forcePosition.position, _forcePosition.up, _force);
             _otherWheel.UpdateSpeed(_t);
         }
 
@@ -35,7 +38,7 @@ namespace Cars
             if (_currentLine != null) _currentLine.UpdateLine();
             _t = Mathf.Clamp(_t - Time.deltaTime * _speed, 0f, 1f);
             var em = _particleSystem.emission;
-            em.rateOverTime = Mathf.Clamp(_wheelCollider.rpm - 1f, 0f, 500f) / 10f;
+            em.rateOverTime = Mathf.Clamp(_wheelCollider.rpm - 1f, 0f, 500f) / 5f;
         }
 
         public void UpdateSpeed(float value)
