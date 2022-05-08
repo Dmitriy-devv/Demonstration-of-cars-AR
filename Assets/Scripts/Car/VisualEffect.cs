e47551db1576107a3f04acd5d789b4b7d8b68a0d
Dmitry Malinkin <malinkin20001@gmail.com>
2022-05-08 19:39:44 +0300

    Accumulator particle/light/sound effects

--- Assets/Scripts/Car/VisualEffect.cs
+++ Assets/Scripts/Car/VisualEffect.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class VisualEffect : MonoBehaviour
 {
     [SerializeField] private float _timeToDestroy;
-
+    [SerializeField] private GameObject _objToActive;
     private ParticleSystem _particleSystem;
     private bool _isInit = false;
 
@@ -19,7 +20,7 @@ public class VisualEffect : MonoBehaviour
     public void Play()
     {
         if (!_isInit) Init();
-
+        if(_objToActive != null) _objToActive.SetActive(true);
         _particleSystem.Play(true);
         StartCoroutine(DelayDestroy());
     }
@@ -27,7 +28,7 @@ public class VisualEffect : MonoBehaviour
     private IEnumerator DelayDestroy()
     {
         yield return new WaitForSeconds(_timeToDestroy);
-
+        
         Destroy(gameObject);
     }
 }
